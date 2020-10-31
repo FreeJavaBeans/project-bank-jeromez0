f96d55ee0f9ba24f0f42481b0168d316c0efeff0
estrangedwriter <jeromezhang1996@gmail.com>
2020-10-31 10:03:44 -0700

    Database design

--- DatabaseScript.sql
+++ DatabaseScript.sql
@@ -1,3 +1,61 @@
--- SQL Script to set up the database for the Banking Application
+/*******************************************************************************
+   BankApplication Database
+   Script: DatabaseScript.sql
+   Description: Creates and populates the Bank App database.
+   DB Server: PostgreSql
+   Author: Jerome Zhang
+********************************************************************************/
 
-set schema BankProject;
+--Create Tables
+set schema 'BankApplication';
+
+drop table if exists "Customers";
+drop table if exists "Employees";
+drop table if exists "BankAccounts";
+drop table if exists "UserAuth";
+
+
+create table "UserAuth" 
+(
+	"KeyID" serial primary key,	
+	"Username" varchar(25) not null,
+	"Password" varchar(25) not null
+);
+
+create table "BankAccounts"
+(
+	"KeyID" serial primary key,	
+	"AccountID" varchar(16) not null,
+	"RoutingID" varchar(16) not null,
+	"Balance" numeric(10,2) not null,
+	"Address" varchar(100),
+	"Approval" boolean,
+	"DOB" timestamp,
+	foreign key ("KeyID")
+		references "UserAuth" ("KeyID")
+);
+
+
+create table "Employees"
+(
+	"KeyID" serial primary key,	
+	"FirstName" varchar(40) not null,
+	"LastName" varchar(40) not null,
+	"Email" varchar(60) not null,
+	"Address" varchar(100),
+	"DOB" timestamp,
+	foreign key ("KeyID")
+		references "UserAuth" ("KeyID")
+);
+
+create table "Customers"
+(
+	"KeyID" serial primary key,	
+	"LastName" varchar(40) not null,
+	"Email" varchar(60) not null,
+	"Address" varchar(100),
+	"DOB" timestamp,
+	foreign key ("KeyID")
+		references "UserAuth" ("KeyID")
+);
+
