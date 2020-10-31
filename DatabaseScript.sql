53b18b6e782ecbca7ad68a160a63ada63ffec172
estrangedwriter <jeromezhang1996@gmail.com>
2020-10-31 10:10:53 -0700

    Further database optimization

--- DatabaseScript.sql
+++ DatabaseScript.sql
@@ -7,11 +7,13 @@
 ********************************************************************************/
 
 --Create Tables
+--==================
+--create schema 'BankApplication';
 set schema 'BankApplication';
 
-drop table if exists "Customers";
 drop table if exists "Employees";
 drop table if exists "BankAccounts";
+drop table if exists "Customers";
 drop table if exists "UserAuth";
 
 
@@ -21,20 +23,6 @@ create table "UserAuth"
 	"Username" varchar(25) not null,
 	"Password" varchar(25) not null
 );
-
-create table "BankAccounts"
-(
-	"KeyID" serial primary key,	
-	"AccountID" varchar(16) not null,
-	"RoutingID" varchar(16) not null,
-	"Balance" numeric(10,2) not null,
-	"Address" varchar(100),
-	"Approval" boolean,
-	"DOB" timestamp,
-	foreign key ("KeyID")
-		references "UserAuth" ("KeyID")
-);
-
 
 create table "Employees"
 (
@@ -51,6 +39,7 @@ create table "Employees"
 create table "Customers"
 (
 	"KeyID" serial primary key,	
+	"FirstName" varchar(40) not null,
 	"LastName" varchar(40) not null,
 	"Email" varchar(60) not null,
 	"Address" varchar(100),
@@ -58,4 +47,16 @@ create table "Customers"
 	foreign key ("KeyID")
 		references "UserAuth" ("KeyID")
 );
+
+create table "BankAccounts"
+(
+	"KeyID" serial primary key,	
+	"AccountID" varchar(16) not null,
+	"RoutingID" varchar(16) not null,
+	"Balance" numeric(10,2) not null,
+	"Approval" boolean,
+	foreign key ("KeyID")
+		references "Customers" ("KeyID")
+);
+
 
